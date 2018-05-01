b8306a3b8283889d56a5df9654cf8d375a8ce286
Frédéric Minatchy <frederic.minatchy@videotron.com>
2018-05-01 00:09:21 -0400

    Creation du model de données Item

---
+++ src/main/java/fr/frederic/evolucity/item/manager/model/Item.java
@@ -0,0 +1,88 @@
+package fr.frederic.evolucity.item.manager.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "EVO_ITEMS")
+public class Item {
+
+
+	@Id 
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name = "ID")
+	private long id;
+
+	@Column(name="pname")
+	private String name;
+
+	@Column
+	private int quantity;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + quantity;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (quantity != other.quantity)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Item [id=" + id + ", name=" + name + ", quantity=" + quantity + "]";
+	}
+
+	
+
+}
