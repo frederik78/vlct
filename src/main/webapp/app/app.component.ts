d2705d71c01e336fa526115e0eecf90f35ad6342
Frédéric Minatchy <frederic.minatchy@videotron.com>
2018-05-01 23:44:49 -0400

    Ajout de la librairie Ag_Grid

--- src/main/webapp/app/app.component.ts
+++ src/main/webapp/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import {AgGridUtil} from './shared/ag-grid/ag-grid-utils';
+import {GridOptions} from 'ag-grid';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  gridOptions: GridOptions;
+
+  constructor() {
+    this.gridOptions = AgGridUtil.getNewAgGridOptionsInstance();
+    this.gridOptions.columnDefs = [
+      {headerName: 'Make', field: 'make'},
+      {headerName: 'Model', field: 'model'},
+      {headerName: 'Price', field: 'price'}
+    ];
+
+
+  }
+
+  ngOnInit(): void {
+    this.gridOptions.rowData = [
+      {make: 'Toyota', model: 'Celica', price: 35000},
+      {make: 'Ford', model: 'Mondeo', price: 32000},
+      {make: 'Porsche', model: 'Boxter', price: 72000}
+    ];
+  }
+
+
 }
---
+++ src/main/webapp/app/shared/ag-grid/ag-grid-utils.ts
@@ -0,0 +1,40 @@
+import {GridOptions} from "ag-grid";
+export class AgGridUtil {
+
+    public static getNewAgGridOptionsInstance(): GridOptions {
+
+
+        let gridOptions = <GridOptions>{
+            rowSelection: 'single',
+            enableSorting: true,
+            enableColResize: true,
+            enableFilter: true,
+            pagination: true,
+            paginationAutoPageSize: true,
+            suppressDragLeaveHidesColumns: true,
+            overlayLoadingTemplate: '<span class="ag-overlay-loading-center">Chargement en cours</span>',
+            overlayNoRowsTemplate: '<span style="padding: 10px; border: 2px solid #444; background: lightgoldenrodyellow;">Aucun enregistrement</span>',
+            rowData: null,
+        };
+
+        gridOptions.localeText = {
+
+            // for number filter
+            equals: 'Égal',
+            notEqual: 'Pas égal',
+            lessThanOrEqual: 'Moins que ou égal',
+            greaterThanOrEqual: 'Plus que ou égal',
+            inRange: 'Dans la portée',
+            lessThan: 'Moins que',
+            greaterThan: 'Plus que',
+
+            // for text filter
+            contains: 'Contient',
+            notContains: 'Ne contient pas',
+            startsWith: 'Commence avec',
+            endsWith: 'Termine avec'
+        };
+        return gridOptions;
+    }
+
+}
