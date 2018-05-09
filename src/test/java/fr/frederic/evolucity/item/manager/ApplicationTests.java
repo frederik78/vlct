e852b45c99330c91dd4065326d152d1e3f6f616c
Frédéric Minatchy <Frederic.Minatchy@videotron.com>
2018-05-09 09:24:36 -0400

    Stabilisation test unitaire

--- src/test/java/fr/frederic/evolucity/item/manager/ApplicationTests.java
+++ src/test/java/fr/frederic/evolucity/item/manager/ApplicationTests.java
@@ -5,8 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import fr.frederic.evolocity.item.manager.Application;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes= {Application.class})
 public class ApplicationTests {
 
 	@Test
